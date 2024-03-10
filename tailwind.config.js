edd449f83d2ee46a6cb7c193136a31bb738ed429
danihermes <danielhn@gmail.com>
2024-03-10 19:50:08 -0400

    update tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
     content: ["./src/**/*.{html,js}", "./index.html", './{html,js}'],
     theme: {
@@ -9,8 +11,24 @@ module.exports = {
             'mindaro': '#FCFDAF',
             'flax': '#EFD780',
             'earth-yellow': '#DBA159'
-        }
+        },
+        textShadow: {
+          sm: '0 1px 2px var(--tw-shadow-color)',
+          DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+          lg: '0 8px 16px var(--tw-shadow-color)',
+        },
       },
     },
-    plugins: [],
+    plugins: [
+      plugin(function ({ matchUtilities, theme }) {
+        matchUtilities(
+          {
+            'text-shadow': (value) => ({
+              textShadow: value,
+            }),
+          },
+          { values: theme('textShadow') }
+        )
+      }),
+    ],
   }
